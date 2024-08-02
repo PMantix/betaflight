2fe839d37e4170b9deebeeec2cf93cd9c76bf143
PMantix <PMantix@gmail.com>
2024-08-01 21:12:52 -0400

    Fixed typo, 'micer' -> 'mixer'

--- src/main/flight/mixer_init.c
+++ src/main/flight/mixer_init.c
@@ -367,7 +367,7 @@ void mixerInitProfile(void)
     mixerRuntime.rpmLimiterI = 0.0;
     pt1FilterInit(&mixerRuntime.rpmLimiterAverageRpmFilter, pt1FilterGain(mixerConfig()->rpm_limit_rpm_filter_cutoff, pidGetDT()));
     pt1FilterInit(&mixerRuntime.rpmLimiterThrottleScaleOffsetFilter, pt1FilterGain(micerConfig()->rpm_limit_thr_scale_cutoff, pidGetDT()));
-    pt1FilterInit(&mixerRuntime.rpmLimiterDTermFilter, pt1FilterGain(micerConfig()->rpm_limit_d_term_cutoff, pidGetDT()));
+    pt1FilterInit(&mixerRuntime.rpmLimiterDTermFilter, pt1FilterGain(mixerConfig()->rpm_limit_d_term_cutoff, pidGetDT()));
     mixerResetRpmLimiter();
 #endif
 
